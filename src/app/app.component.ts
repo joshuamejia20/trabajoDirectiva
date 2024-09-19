9c17808d71f6758dffde056298dec205ea6c96c7
Josue Mejia <josueomejiap@gmail.com>
2024-09-19 08:43:50 -0600

    clase 180924

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectiva';
+  titulo = 'Registro de Usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre = "";
+  apellido = "";
+  entradas: any[];
+
+  constructor(){
+    this.entradas =[
+      {
+        titulo: "Python, cada día más presente"
+      },
+      {
+        titulo: "JAVA, presente en más de 20 años"
+      },
+      {
+        titulo: "JavaScript, cada vez más funcional"
+      }
+    ];
+  }
+
+  registrar_usuario(){
+    this.mensaje = "Usuario registrado exitosamente";
+    this.registrado = true;
+  }
 }
